fbf9cc7747a2341cc4fc6a9cb25f579b7f7c925f
Aaron A. King <kingaa@umich.edu>
2014-08-12 09:31:17 -0400

    - add 'scinot' command

---
+++ aakmisc/R/scinot.R
@@ -0,0 +1,20 @@
+scinot <- function (x, digits = 2, type = c("expression","latex","math")) {
+  if (length(x) > 1)
+    warning("only the first element of ",sQuote("x")," will be formatted")
+  type <- match.arg(type)
+  x <- signif(x[1],digits=digits)
+  ch <- floor(log10(abs(x)))
+  mn <- x/10^ch
+  switch(
+         type,
+         expression={
+           bquote(.(mn)%*%10^.(ch))
+         },
+         latex={
+           paste0("{",mn,"}{$\\times$}10$^{",ch,"}$")
+         },
+         math={
+           paste0("${",mn,"}{\\times}10^{",ch,"}$")
+         }
+         )
+}
