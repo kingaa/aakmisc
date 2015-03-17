9ecd1ab616775e0a2a4e7d8df560a70a126fbf38
Aaron A. King <kingaa@umich.edu>
2015-03-17 17:05:17 -0400

    - add 'curl' to 'Imports' list - move DBI and RPostgreSQL to 'Imports' instead of 'Depends' - fix 'random.org' to work with https

--- aakmisc/R/random.R
+++ aakmisc/R/random.R
@@ -1,13 +1,13 @@
 ## download random seeds from random.org
 
 random.org <- function (n = 10, rnd = "new") {
-  template <- "http://www.random.org/integers/?num=%d&min=%d&max=%d&col=%d&base=%d&format=plain&rnd=%s"
+  template <- "https://www.random.org/integers/?num=%d&min=%d&max=%d&col=%d&base=%d&format=plain&rnd=%s"
   base <- 2
   min <- 0
   max <- 1
   col <- 31
   query <- sprintf(template,n*col,min,max,col,base,rnd)
-  con <- url(query)
+  con <- curl::curl(query)
   dat <- scan(con,what=character(0))
   close(con)
   dim(dat) <- c(n,col)
