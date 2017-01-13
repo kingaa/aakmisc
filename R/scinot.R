ff11a7b5ee7639243bc363dd1a072832f8414d5b
Aaron A. King <kingaa@umich.edu>
2017-01-13 13:53:10 -0500

    add 'simplify' option to 'scinot'

--- R/scinot.R
+++ R/scinot.R
@@ -1,20 +1,30 @@
-scinot <- function (x, digits = 2, format = c("expression","latex","math")) {
+scinot <- function (x, digits = 2, format = c("expression","latex","math"),
+                    simplify = FALSE) {
   if (length(x) > 1)
     warning("only the first element of ",sQuote("x")," will be formatted")
   format <- match.arg(format)
-  x <- signif(x[1],digits=digits)
+  x <- signif(as.numeric(x[1]),digits=digits)
   ch <- floor(log10(abs(x)))
   mn <- x/10^ch
   switch(
-         format,
-         expression={
-           bquote(.(mn)%*%10^.(ch))
-         },
-         latex={
-           paste0("{",mn,"}{$\\times$}10$^{",ch,"}$")
-         },
-         math={
-           paste0("${",mn,"}{\\times}10^{",ch,"}$")
-         }
-         )
+      format,
+      expression={
+          if (mn == 1)
+              bquote(10^.(ch))
+          else
+              bquote(.(mn)%*%10^.(ch))
+      },
+      latex={
+          if (mn == 1)
+              paste0("10$^{",ch,"}$")
+          else
+              paste0("{",mn,"}{$\\times$}10$^{",ch,"}$")
+      },
+      math={
+          if (mn == 1)
+              paste0("$10^{",ch,"}$")
+          else
+              paste0("${",mn,"}{\\times}10^{",ch,"}$")
+      }
+  )
 }
